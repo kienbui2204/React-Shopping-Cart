78bfa54fa27bc464aa66ec6c7f1dc3cf35fd8015
kien trung <kien.bui@itbeesolutions.vn>
2021-08-08 19:37:28 +0700

    Checkout form

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,6 +2,32 @@
 import formatCurrency from '../ultil';
 
  export default class Cart extends Component {
+     constructor(props) {
+         super(props);
+         this.state = {
+             name: "",
+             email: "",
+             address: "",
+             showCheckout: false,
+            };
+     }
+     handleClick = (e) =>{
+        this.setState({
+            [e.target.name] : e.target.value
+        });
+    }
+
+    createOrder = (e) => {
+        e.preventDefault();
+        const order = {
+            name: this.state.name,
+            email: this.state.email,
+            address: this.state.address,
+            cartItems: this.state.cartItems
+        };
+        this.props.createOrder(order);
+    }
+    
      render() {
          const {cartItems} = this.props;
          return (
@@ -24,7 +50,7 @@ import formatCurrency from '../ultil';
                                   <div>
                                       <div>{item.title}</div>
                                       <div className="right">
-                                          {formatCurrency(item.price)} x {item.count} 
+                                          {formatCurrency(item.price)} x {item.count}
                                         <button className="button" onClick={() => this.props.removeFromCart(item)}>
                                             Remove
                                             </button>
@@ -36,6 +62,7 @@ import formatCurrency from '../ultil';
                       </ul>
                   </div>
                   {cartItems.length !== 0 && (
+                      <div>
                     <div className="cart">
                       <div className="total">
                           <div>
@@ -44,13 +71,36 @@ import formatCurrency from '../ultil';
                                   cartItems.reduce((a, c) => a + c.price * c.count, 0)
                               )}
                           </div>
-                          <button className="button primary">
+                          <button onClick={() => this.setState({showCheckout: true})} className="button primary">
                               Proceed
                           </button>
                       </div>
                   </div>
+                    {this.state.showCheckout && (
+                        <div className="cart">
+                            <form onSubmit={this.createOrder}>
+                                <ul className="form-container">
+                                    <li>
+                                        <label>Email</label>
+                                        <input name="email" type="email" required onChange={this.handleInput}/>
+                                    </li>
+                                    <li>
+                                        <label>Name</label>
+                                        <input name="name" type="text" required onChange={this.handleInput}/>
+                                    </li>
+                                    <li>
+                                        <label>Address</label>
+                                        <input name="address" type="text" required onChange={this.handleInput}/>
+                                    </li>
+                                    <li>
+                                        <button className="button primary" type="submit">Checkout</button>
+                                    </li>
+                                </ul>
+                            </form>
+                        </div>
+                    )}
+                    </div>
                   )}
-
                 </div>
              </div>
 
