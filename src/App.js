ed8a159de9b7ad775b426d84cd74f44f3daf04d6
kien trung <kien.bui@itbeesolutions.vn>
2021-08-08 14:10:42 +0700

    Create products component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 // Featuer 1
-function App() {
+import React, { Component } from 'react'
+import Products from './components/Products';
+import data from './data.json';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    }
+  }
+
+  render() {
   return (
     <div className="grid-container">
       <header>
         <a href="/">React Shopping Cart</a>
       </header>
       <main>
-        Product List
+        <div className="content">
+          <div className="main">
+            <Products products={this.state.products} />
+          </div>
+          <div className="sidebar">
+            Cart Item
+          </div>
+        </div>
       </main>
       <footer>
         All rights reserved.
       </footer>
     </div>
-  );
+  )
+  }
 }
-
-export default App;
---
+++ src/components/Products.js
@@ -0,0 +1,27 @@
+ import React, { Component } from 'react'
+import formatCurrency from '../ultil'
+
+ export default class Products extends Component {
+     render() {
+         return (
+             <div>
+                 <ul className="products">
+                     {this.props.products.map(product => (
+                         <li key={product._id}>
+                             <div className="product">
+                                 <a href="#">
+                                     <img src={product.image} alt="product image" />
+                                     <p>{product.title}</p>
+                                 </a>
+                             </div>
+                             <div className="product-price">
+                                 <div>{formatCurrency(product.price)}</div>
+                                 <button className="button primary">Add To Cart</button>
+                             </div>
+                         </li>
+                     ))}
+                 </ul>
+             </div>
+         )
+     }
+ }
